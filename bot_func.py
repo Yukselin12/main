b3c098130327ffd16578e7e22b1f96a2ebddda11
Yukselin12 <andybaris15@gmail.com>
2024-09-30 11:06:18 -0500

    Create bot_func.py

---
+++ bot_func.py
@@ -0,0 +1,26 @@
+import random
+import requests
+
+def gen_pass(pass_length):
+    elements = "+-/*!&$#?=@<>"
+    password = ""
+
+    for i in range(pass_length):
+        password += random.choice(elements)
+
+    return password
+def gen_emodji():
+    emodji = [":relaxed:", ":angry:", ":smile:", "\U0001F923"]
+    return random.choice(emodji)
+
+def get_duck_image_url():    
+    url = 'https://random-d.uk/api/random'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+
+def get_dog_image_url():    
+    url = 'https://random.dog/woof.json'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
